65e4a517f383824135c28ab999beab748bb08d8d
mikaelvery <mikael.very34@gmail.com>
2024-10-10 21:18:18 +0200

    setup gallery and style

--- app/_components/ApplicationGallery.tsx
+++ app/_components/ApplicationGallery.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Section } from "./Section";
 import { Card } from "@/components/ui/card";
 
@@ -6,22 +7,22 @@ const ApplicationGallery = () => {
         {
             title: "Sheep Care",
             description: "Manage your sheep farm with stock and parcel management.",
-            previewImage: "/loading.png",
+            previewImage: "/SheepCareGallery.png",
             detailPageUrl: "/applications/sheep-care",
             githubUrl: "https://github.com/mikaelvery/sheep_care",
         },
         {
             title: "Pizza App",
             description: "Real-time GPS tracking for pizza delivery.",
-            previewImage: "/PizzaApp.png",
+            previewImage: "/PizzaAppGallery.png",
             detailPageUrl: "/applications/pizza-app",
             githubUrl: "https://github.com/mikaelvery/pizza_app",
         },
     ];
 
     return (
-        <Section className="flex flex-col gap-4">
-            <h2 className="text-2xl font-semibold">Mes Applications</h2>
+        <Section className="flex flex-col items-start gap-4">
+            <Badge variant={"outline"}>Last Projects</Badge>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {APPLICATIONS.map((app, index) => (
                     <Card key={index} className="flex flex-col items-center p-4 shadow-md hover:shadow-lg transition-shadow duration-200">
@@ -29,10 +30,7 @@ const ApplicationGallery = () => {
                         <h3 className="text-lg font-semibold">{app.title}</h3>
                         <p className="text-sm text-muted-foreground">{app.description}</p>
                         <div className="mt-2">
-                            <a href={app.detailPageUrl} className="text-blue-600 hover:underline">Voir Détails</a>
-                        </div>
-                        <div className="mt-1">
-                            <a href={app.githubUrl} className="text-blue-600 hover:underline">GitHub</a>
+                            <a href={app.detailPageUrl} className="text-white hover:underline">See more</a>
                         </div>
                     </Card>
                 ))}
