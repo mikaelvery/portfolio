9d5aaa963c7b33640c910f17c0f05e21cd91a9e9
mikaelvery <mikael.very34@gmail.com>
2025-03-12 01:56:12 +0100

    change callback

--- app/api/auth/callback.js
+++ app/api/auth/callback.js
@@ -1,33 +1,30 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const { code } = req.query; // Récupère le code OAuth envoyé par Twitch
+  if (req.method === 'GET') {
+    const { code } = req.query;
+    if (!code) {
+      return res.status(400).json({ error: 'Code missing' });
+    }
 
-  if (!code) {
-    return res.status(400).json({ error: 'Code missing' });
-  }
+    try {
+      const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
+        params: {
+          client_id: process.env.TWITCH_CLIENT_ID,
+          client_secret: process.env.TWITCH_CLIENT_SECRET,
+          code,
+          grant_type: 'authorization_code',
+          redirect_uri: process.env.API_CALLBACK_URL,
+        }
+      });
 
-  try {
-    // Échange le code contre un access token
-    const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
-      params: {
-        client_id: process.env.TWITCH_CLIENT_ID,
-        client_secret: process.env.TWITCH_CLIENT_SECRET,
-        code,
-        grant_type: 'authorization_code',
-        redirect_uri: process.env.API_CALLBACK_URL, // URL de callback
-      },
-    });
-
-    // enregistre cet access token pour l'utiliser plus tard
-    const accessToken = response.data.access_token;
-    const refreshToken = response.data.refresh_token;
-
-    // Gestion du token dans la base de données ou le stocke dans la session, par exemple.
-    res.status(200).json({ accessToken, refreshToken });
-
-  } catch (error) {
-    console.error('Erreur lors de la récupération du token Twitch:', error);
-    res.status(500).json({ error: 'Error fetching access token' });
+      const accessToken = response.data.access_token;
+      res.status(200).json({ message: 'Success', accessToken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
